28cbf661e3cefe0fa578079346ebc09f38ae22f2
Marcel Chwialkowski <marcel.chwialkowski@polytechnique.edu>
2024-11-28 21:56:47 +0100

    Cleanup + notebooks uploaded.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -7,7 +7,6 @@ from . import scrapping
 from . import visualize
 from . import basicstatistics
 from . import transportationtool
-from ..relics import otptransport
 
 #to assure that reload(helpers) reloads everything in the folder.
 from importlib import reload
@@ -15,13 +14,11 @@ reload(scrapping)
 reload(visualize)
 reload(basicstatistics)
 reload(transportationtool)
-reload(otptransport)
 
 #to not have to import each file separetly.
 from .scrapping import *
 from .visualize import *
 from .basicstatistics import *
 from .transportationtool import *
-from ..relics.otptransport import *
 
 print("--- Transportation Module loaded ---")
